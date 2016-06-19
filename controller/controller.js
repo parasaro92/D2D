253788362d0ecb5aa912dc599f5668e0f63687e7
paras <rockypro92@gmail.com>
2016-06-19 16:58:48 +0530

    Created another directive and connected both direcitives

--- controller/controller.js
+++ controller/controller.js
@@ -11,9 +11,38 @@ myApp.directive('tab', function() {
   return {
     restrict: 'E',
     transclude: true,
-    template: '<h2>Hello world!</h2> <div role="tabpanel" ng-transclude></div>',
-    scope: { },
-    link: function(scope, elem, attr) {}
+    template: '<h2>Hello world!</h2> <div role="tabpanel" ng-show="active" ng-transclude></div>',
+    require: '^tabset',
+    scope: { 
+      heading: '@'
+    },
+    link: function(scope, elem, attr, tabsetCtrl) {
+      scope.active = false;
+      tabsetCtrl.addTab(scope);
+    }
   }
 });
 
+myApp.directive('tabset', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: { },
+    templateUrl: 'views/tabset.html',
+    bindToController: true,
+    controllerAs: 'tabset',
+    controller: function() {
+      var vm = this
+      vm.tabs = []
+
+      vm.addTab = function addTab(tab){
+
+        vm.tabs.push(tab);
+
+        if(vm.tabs.length === 1){
+          tab.active = true
+        }
+      }
+    }
+  }
+});
